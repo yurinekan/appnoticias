852ef6959581e814e01f8ca11e549519e6ddf125
Yuri Nekan <yurinekanfts@hotmail.com>
2019-09-19 18:20:00 -0300

    É corra viu

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,10 +7,25 @@ import { NoticiasService } from '../noticias.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
+  usa: any = [];
+  bre = 0;
+  tec: any = [];
+  ent = 0;
+  hlt: any = [];
 
-  data: any;
+  datahlt: any;
+  datausa: any;
+  databre: any;
+  datatec: any;
+  dataent: any;
+
+  nHLT = false;
+  nENT = false;
+  nTEC = false;
   nUSA = false;
+  nBRE = false;
   hideAll = false;
+
   slideOpts = {
     slidesPerView: 1,
     initialSlide: 0,
@@ -18,20 +33,125 @@ export class Tab2Page implements OnInit {
     pagination: {
       el: '.swiper-pagination',
       type: 'progressbar',
-  }
+    }
   };
-  constructor(private noticiaService: NoticiasService) {}
+  constructor(private noticiaService: NoticiasService) { }
 
   ngOnInit() {
-    this.noticiaService
-      .getData(`top-headlines?country=us&pageSize=100`)
-      .subscribe(dados => {
-        console.log(dados);
-        this.data = dados;
-      });
-    }
+    // setTimeout(() => {
+    //   this.nBRE = false;
+    //   this.nUSA = false;
+    //   // event.target.complete();
+    // }, 350);
+  }
 
-  appearUSA() {
-    this.nUSA = !this.nUSA;
+  async appearUSA() {
+    this.usa++;
+
+    if (this.usa === 1) {
+      this.noticiaService
+        .getData(`top-headlines?country=us&pageSize=100`)
+        .subscribe(dados => {
+          console.log(dados);
+          this.datausa = dados;
+        });
+    }
+    if ( this.nBRE === false && this.nTEC === false  && this.nENT === false && this.nHLT === false) {
+      this.nUSA = !this.nUSA;
+    } else {
+      this.nBRE = false;
+      this.nTEC = false;
+      this.nENT = false;
+      this.nHLT = false;
+      this.nUSA = !this.nUSA;
+    }
+  }
+
+  appearBRE() {
+    this.bre++;
+
+    if (this.bre === 1) {
+      this.noticiaService
+        .getData(`top-headlines?country=br&category=sports&pageSize=100`)
+        .subscribe(dados => {
+          console.log(dados);
+          this.databre = dados;
+        });
+    }
+    if (this.nUSA === false && this.nTEC === false  && this.nENT === false && this.nHLT === false) {
+      this.nBRE = !this.nBRE;
+    } else {
+      this.nUSA = false;
+      this.nTEC = false;
+      this.nENT = false;
+      this.nHLT = false;
+      this.nBRE = !this.nBRE;
+    }
+  }
+
+  appearTEC() {
+    this.tec++;
+
+    if (this.tec === 1) {
+      this.noticiaService
+        .getData(`top-headlines?country=br&category=technology&pageSize=100`)
+        .subscribe(dados => {
+          console.log(dados);
+          this.datatec = dados;
+        });
+    }
+    if (this.nBRE === false && this.nUSA === false && this.nENT === false && this.nHLT === false) {
+      this.nTEC = !this.nTEC;
+    } else {
+      this.nBRE = false;
+      this.nUSA = false;
+      this.nENT = false;
+      this.nHLT = false;
+      this.nTEC = !this.nTEC;
+    }
+  }
+
+  appearENT() {
+    this.ent++;
+
+    if (this.ent === 1) {
+      this.noticiaService
+        .getData(`top-headlines?country=br&category=entertainment&pageSize=100`)
+        .subscribe(dados => {
+          console.log(dados);
+          this.dataent = dados;
+        });
+    }
+    if (this.nBRE === false && this.nUSA === false && this.nTEC === false && this.nHLT === false) {
+      this.nENT = !this.nENT;
+    } else {
+      this.nBRE = false;
+      this.nUSA = false;
+      this.nTEC = false;
+      this.nHLT = false;
+      this.nENT = !this.nENT;
+    }
+  }
+
+  appearHLT() {
+    this.hlt++;
+
+    if (this.hlt === 1) {
+      this.noticiaService
+        .getData(`top-headlines?country=br&category=health&pageSize=100`)
+        .subscribe(dados => {
+          console.log(dados);
+          this.datahlt = dados;
+        });
+    }
+    if (this.nBRE === false && this.nUSA === false && this.nTEC === false && this.nENT === false) {
+      this.nHLT = !this.nHLT;
+    } else {
+      this.nBRE = false;
+      this.nUSA = false;
+      this.nTEC = false;
+      this.nENT = false;
+      this.nHLT = !this.nHLT;
+    }
   }
 }
